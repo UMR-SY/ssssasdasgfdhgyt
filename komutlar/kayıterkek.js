f68eca8cabdc848fe3b434b92d72aefdd3513499
Glitch (brave-paw) <none>
2020-03-04 19:05:23 +0000

    🐫🍜 Checkpoint ./komutlar/kayıterkek.js:11800395/2224

--- komutlar/kayıterkek.js
+++ komutlar/kayıterkek.js
@@ -11,14 +11,13 @@ exports.run = async (client, message, args) => {
   if (!member) return message.channel.send(":x: Bir Üye Etiketlemelisin!");
   if (!isim) return message.channel.send(":x: Bir İsim Yazmalısın!");
   member.setNickname(`${isim}`);
-  member.removeRole('679762613659631674')
-  member.addRole('679757249836875789')
-const embed = new Discord.RichEmbed()
-
-
-      .addField(`**🏷 Brigthness Kayıt 🏷**`,
-      `\n**🔸️Kayıt Edilen Kullanıcı:** ${member.user} \n🔸️**Kayıt Eden Yetkili:** \`${message.author.username}\``)
-client.channels.get('680872181370060911').send(embed)
+  member.removeRole("679762613659631674");
+  member.addRole("679757249836875789");
+  const embed = new Discord.RichEmbed().addField(
+    `**🏷 Brigthness Kayıt 🏷**`,
+    `\n**🔸️Kayıt Edilen Kullanıcı:** ${member.user} \n🔸️**Kayıt Eden Yetkili:** \`${message.author.username}\``
+  );
+  client.channels.get("680872181370060911").send(embed);
 };
 
 exports.conf = {
